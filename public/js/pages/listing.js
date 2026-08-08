479ee430ba2be7f523b262b3e9e94b0710132f38
Liangquan Wu <liangquan.professional@gmail.com>
2026-08-08 17:03:49 -0400

    Create reusable modal component and add description and owner section to listing

--- public/js/pages/listing.js
+++ public/js/pages/listing.js
@@ -1,6 +1,11 @@
 import { createNavbar } from "../components/navbar.js";
 import { createPropertyListingTop } from "../components/propertyListingTop.js";
+import { createPropertySummary } from "../components/propertySummary.js";
+import { createHostSection } from "../components/hostSection.js";
+import { createPropertyDescription } from "../components/propertyDescription.js";
+
 import { loadPropertyById, loadPropertyImages, loadPropertyAmenities, loadPropertyReviews } from "../services/propertyService.js"
+
 
 const params = new URLSearchParams(window.location.search);
 const listingId = params.get("id");
@@ -26,6 +31,10 @@ const amenities = await loadPropertyAmenities(`/api/properties/${listingId}/amen
 const reviews = await loadPropertyReviews(`/api/properties/${listingId}/reviews`);
 
 const top = createPropertyListingTop(property, images);
+const bodyLeftSummary = createPropertySummary(property);
+const bodyLeftHost = createHostSection(property);
+const bodyLeftDescription = createPropertyDescription(property);
 
 topContainer.append(top);
+leftBodyContainer.append(bodyLeftSummary, bodyLeftHost, bodyLeftDescription);
 
