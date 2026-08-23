3da206e9c87208567bb11c35eae7dfca573f90b5
Max Chiu <mc4487@drexel.edu>
2026-08-22 20:59:40 -0400

    Add date module and start working on calendar

--- public/js/pages/hostCalendar.js
+++ public/js/pages/hostCalendar.js
@@ -1,10 +1,24 @@
 import { createNavbar } from "../components/navbar.js";
+import { 
+  getMonth,
+  getYear,
+  startOfToday 
+} from "https://cdn.jsdelivr.net/npm/date-fns/+esm";
 
+
+// Navbar
 const navBarContainer = document.getElementById("navbar-container");
-
 const navElement = createNavbar({
   userMode: "host",
   activeHostTab: 1,
 });
-
 navBarContainer.appendChild(navElement);
+
+// Current Date
+
+let date = startOfToday();
+let month = getMonth(date);
+let year = getYear(date);
+console.log(date);
+console.log(month);
+console.log(year);
