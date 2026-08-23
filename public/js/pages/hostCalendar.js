39c40f3b67a8e83f94531b5571c40cc894819af9
Max Chiu <mc4487@drexel.edu>
2026-08-23 01:57:39 -0400

    Dropdowns

--- public/js/pages/hostCalendar.js
+++ public/js/pages/hostCalendar.js
@@ -21,12 +21,66 @@ let date = startOfToday();
 let month = getMonth(date);
 let year = getYear(date);
 
-let calendarHeader = document.getElementById("calendar-header");
-calendarHeader.textContent = format(date, "MMMM");
+const monthIndexToName = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December"
+}
 
-const monthDropdown = document.getElementById("month-dropdown");
+const monthYearDropdown = document.getElementById("month-year-dropdown");
+const chooseMonthOrYear = document.getElementById("calendar-filter");
 
-monthDropdown.addEventListener("change", (event) => {
-    const month = Number(event.target.value);
-    console.log(month);
+function setMonthDropdown() {
+  monthYearDropdown.innerHTML = "";
+
+  for (const [index, name] of Object.entries(monthIndexToName)) {
+    const option = document.createElement("option");
+
+    option.value = index;
+    option.textContent = name;
+
+    monthYearDropdown.appendChild(option);
+  }
+
+  monthYearDropdown.value = month;
+}
+function setYearDropdown() {
+  monthYearDropdown.innerHTML = "";
+
+  for (const yearOption of [year-1, year, year+1]) {
+    const option = document.createElement("option");
+
+    option.value = yearOption;
+    option.textContent = yearOption;
+
+    monthYearDropdown.appendChild(option);
+  }
+
+  monthYearDropdown.value = year;
+}
+
+chooseMonthOrYear.addEventListener("change", (event) => {
+  const selection = event.target.value;
+
+  if (selection === "month") {
+    setMonthDropdown();
+  } else if (selection === "year") {
+    setYearDropdown();
+  }
+});
+
+setMonthDropdown();
+chooseMonthOrYear.value = "month";
+
+monthYearDropdown.addEventListener("change", (event) => {
+  const selection = Number(event.target.value);
 });
