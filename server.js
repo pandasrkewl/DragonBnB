d9653239c81cccfc0ffc0b55fe4156f4fe0fa8dc
Max Chiu <maxkchiu@gmail.com>
2026-08-26 00:56:01 -0400

    More changes

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
