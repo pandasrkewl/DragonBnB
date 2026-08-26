0a8c0ec34a64be5edbad81c49d9c338512cc0323
Max Chiu <maxkchiu@gmail.com>
2026-08-26 00:51:00 -0400

    Aslo remove duplicate setval

--- sql/schema.sql
+++ sql/schema.sql
@@ -535,8 +535,3 @@ VALUES
     (4, 16, 'Great! When would you like to stay?', '2026-08-01 10:30:00', FALSE, NULL),
     (5, 9, 'Can I bring a dog?', '2026-07-15 15:30:00', TRUE, '2026-07-15 15:35:00'),
     (5, 4, 'Of course! Dogs are welcome here.', '2026-07-15 15:45:00', FALSE, NULL);
-
-SELECT setval(
-    'users_id_seq',
-    (SELECT MAX(id) FROM users)
-);
