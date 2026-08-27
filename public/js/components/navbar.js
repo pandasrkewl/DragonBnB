1fde25eae8a44e94b13765a154e10c0d6c796f8c
Max Chiu <mc4487@drexel.edu>
2026-08-27 18:42:26 -0400

    Fixes

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -476,18 +476,18 @@ export function createNavbar({
 
   const dropdownMenu = createElement("div", { className: "profile-dropdown" }, [
     createElement("a", {
-      href: "/api/messages",
+      href: "/messages",
       className: "dropdown-item",
       textContent: "Messages",
     }),
     createElement("a", {
-      href: "/api/profile",
+      href: "/profile",
       className: "dropdown-item",
       textContent: "Profile",
     }),
   
     createElement("a", {
-      href: "/api/logout",
+      href: "/logout",
       className: "dropdown-item",
       textContent: "Logout",
     }),
